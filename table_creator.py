2b5e98962e82232eda08b10799b5de253b2d8fc2
Draguve <ritwikpatil1998@gmail.com>
2018-08-02 17:15:33 +0530

    get_timetable now prints all subject codes

--- table_creator.py
+++ table_creator.py
@@ -36,15 +36,20 @@ def find_class(ws,class_code):
                 return cell
     return None
 
-def get_timetable(ws,start_cell):
+def get_timetable(ws,name_cell):
     to_skip = 0
-    for row in ws.iter_rows(min_row=start_cell.row+1,max_row=(int(start_cell.row)+2*11),max_col=column_index_from_string(start_cell.column),min_col=column_index_from_string(start_cell.column)):
-        for cell in row:
-            if(to_skip<=0):
-                class_code,class_room,teacher_code,to_skip = get_period(cell)
-                print(class_code)
-            else:
-                to_skip-=1
+    start_cell = name_cell.offset(1,0)
+    for x in range(5):
+        for row in ws.iter_rows(min_row=start_cell.row,max_row=(int(start_cell.row)+2*10),max_col=column_index_from_string(start_cell.column),min_col=column_index_from_string(start_cell.column)):
+            for cell in row:
+                if(to_skip<=0):
+                    class_code,class_room,teacher_code,to_skip = get_period(cell)
+                    print(class_code)
+                else:
+                    to_skip-=1
+                end_cell = cell
+        print("END OF DAY")
+        start_cell = end_cell.offset(1,0)
 
 def get_period(cell):
     #to find the code
