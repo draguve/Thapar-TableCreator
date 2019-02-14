5714a43fb3d2464edce21e59191b284efb68970a
Draguve <ritwikpatil1998@gmail.com>
2019-02-14 14:32:19 +0530

    Pleasing the pycharm gods

--- table_creator.py
+++ table_creator.py
@@ -22,39 +22,41 @@ def find_all_batches(ws):
     class_row = None
     for row in ws.iter_rows(min_row=1, max_col=1, max_row=10):
         for cell in row:
-            if(cell.value != None and cell.value.lower() == "day"):
-                class_row = cell.row+1
+            if cell.value is not None and cell.value.lower() == "day":
+                class_row = cell.row + 1
     if class_row is None:
         return None
     classes = []
     print(class_row)
-    for column in ws.iter_cols(min_row=class_row ,max_col=100,max_row=class_row):
+    for column in ws.iter_cols(min_row=class_row, max_col=100, max_row=class_row):
         for cell in column:
-            if(cell.value!=None):
-                if(cell.value.lower() !="day" and cell.value.lower() != "hours"):
+            if cell.value is not None:
+                if cell.value.lower() != "day" and cell.value.lower() != "hours":
                     classes.append(cell.value)
     return classes
 
-def find_batch(ws,class_code):
+
+def find_batch(ws, class_code):
     class_row = None
     for row in ws.iter_rows(min_row=1, max_col=1, max_row=10):
         for cell in row:
-            if(cell.value != None and cell.value.lower() == "day"):
-                class_row = cell.row+1
+            if cell.value is not None and cell.value.lower() == "day":
+                class_row = cell.row + 1
     if class_row is None:
         return None
-    for column in ws.iter_cols(min_row=class_row ,max_col=100,max_row=class_row):
+    for column in ws.iter_cols(min_row=class_row, max_col=100, max_row=class_row):
         for cell in column:
-            if(cell.value!=None and cell.value.lower() == class_code.lower()):
+            if cell.value is not None and cell.value.lower() == class_code.lower():
                 return cell
     return None
 
-def get_timetable(ws,name_cell):
-    finalworkbook,finalsheet = create_empty_table()
+
+def get_timetable(ws, name_cell):
+    finalworkbook, finalsheet = create_empty_table()
     finalsheet.title = name_cell.value
 
     to_skip = 0
-    start_cell = name_cell.offset(1,0)
+    start_cell = name_cell.offset(1, 0)
 
     daycell = finalsheet["B2"]
     current_cell = daycell
@@ -63,102 +65,109 @@ def get_timetable(ws,name_cell):
     border = Border(top=thin, left=thin, right=thin, bottom=thin)
 
     for x in range(5):
-        for row in ws.iter_rows(min_row=start_cell.row,max_row=(int(start_cell.row)+19),max_col=column_index_from_string(start_cell.column),min_col=column_index_from_string(start_cell.column)):
-            print(" --------------------    "+str(start_cell.row)+"     "+str(start_cell.column))
+        for row in ws.iter_rows(min_row=start_cell.row, max_row=(int(start_cell.row) + 19),
+                                max_col=column_index_from_string(start_cell.column),
+                                min_col=column_index_from_string(start_cell.column)):
             for cell in row:
-                if(to_skip<=0):
-                    #get the period data at the right location
-                    class_code,class_room,teacher_code,to_skip = get_period(cell)
-                    if(class_code!= None and class_code[-1] != "P" and to_skip == 3):
+                if to_skip <= 0:
+                    # get the period data at the right location
+                    class_code, class_room, teacher_code, to_skip = get_period(cell)
+                    if class_code is not None and class_code[-1] != "P" and to_skip == 3:
                         print(cell)
-                    if(class_code!=None and class_room!=None and teacher_code!=None):
+                    if class_code is not None and class_room is not None and teacher_code is not None:
                         to_write = current_cell
                         to_write.value = class_code
                         if to_skip == 3:
-                            to_write.offset(1,0).value = class_room
-                            to_write.offset(2,0).value = "LAB"
-                            to_write.offset(3,0).value = teacher_code
-                            #style
-                            style_range(finalsheet, '{0}{1}:{2}{3}'.format(to_write.column,to_write.row,to_write.offset(3,0).column,to_write.offset(3,0).row), border=border)
-                        else:   
-                            to_write.offset(1,0).value = "%s | %s"%(class_room,teacher_code)
-                            #style
-                            style_range(finalsheet, '{0}{1}:{2}{3}'.format(to_write.column,to_write.row,to_write.offset(1,0).column,to_write.offset(1,0).row), border=border)
-                    current_cell = current_cell.offset(to_skip+1,0) 
+                            to_write.offset(1, 0).value = class_room
+                            to_write.offset(2, 0).value = "LAB"
+                            to_write.offset(3, 0).value = teacher_code
+                            # style
+                            style_range(finalsheet, '{0}{1}:{2}{3}'.format(to_write.column, to_write.row,
+                                                                           to_write.offset(3, 0).column,
+                                                                           to_write.offset(3, 0).row), border=border)
+                        else:
+                            to_write.offset(1, 0).value = "%s | %s" % (class_room, teacher_code)
+                            # style
+                            style_range(finalsheet, '{0}{1}:{2}{3}'.format(to_write.column, to_write.row,
+                                                                           to_write.offset(1, 0).column,
+                                                                           to_write.offset(1, 0).row), border=border)
+                    current_cell = current_cell.offset(to_skip + 1, 0)
                 else:
-                    to_skip-=1
+                    to_skip -= 1
                 end_cell = cell
-        #End of day
+        # End of day
         to_skip = 0
-        start_cell = end_cell.offset(1,0)
-        daycell = daycell.offset(0,1)
+        start_cell = end_cell.offset(1, 0)
+        daycell = daycell.offset(0, 1)
         current_cell = daycell
     return finalworkbook
 
+
 def get_period(cell):
-    #to find the code
+    # to find the code
     class_code = cell.value
-    cell_under=cell.offset(1,0)
+    cell_under = cell.offset(1, 0)
     class_cell = cell
-    if(class_code == None and cell_under.border.left.style == None):
+    if class_code is None and cell_under.border.left.style is None:
         counter = 2
-        while(class_code==None and class_cell.border.left.style != "medium" and counter>0):
-            class_cell = class_cell.offset(0,-1)
+        while class_code is None and class_cell.border.left.style != "medium" and counter > 0:
+            class_cell = class_cell.offset(0, -1)
             class_code = class_cell.value
             counter -= 1
-            if(class_code!= None and class_code[-1] == "T"):
+            if class_code is not None and class_code[-1] == "T":
                 class_code = None
                 break
 
-
-    #find cells to skip
-    if(class_code!=None and class_code[-1] == "P"):
+    # find cells to skip
+    if class_code is not None and class_code[-1] == "P":
         to_skip = 3
     else:
         to_skip = 1
 
-    #find class room
-    if(class_code!=None):
-        class_room = class_cell.offset(1,0).value
+    # find class room
+    if class_code is not None:
+        class_room = class_cell.offset(1, 0).value
     else:
         class_room = None
 
-    #find teacher code 
-    if(to_skip==3):
-        teacher_code = class_cell.offset(3,0).value
-    elif(class_code!=None):
-        teacher_cell = class_cell.offset(1,1)
-        while(teacher_cell.value==None):
-            teacher_cell = teacher_cell.offset(0,1)
+    # find teacher code
+    if to_skip == 3:
+        teacher_code = class_cell.offset(3, 0).value
+    elif class_code is not None:
+        teacher_cell = class_cell.offset(1, 1)
+        while teacher_cell.value is None:
+            teacher_cell = teacher_cell.offset(0, 1)
         teacher_code = teacher_cell.value
     else:
         teacher_code = None
 
-    return class_code,class_room,teacher_code,to_skip
+    return class_code, class_room, teacher_code, to_skip
+
 
 def create_empty_table():
-    #creating a new workbook to store the new timetable
+    # creating a new workbook to store the new timetable
     wb = Workbook()
     finalsheet = wb.active
 
-    #formatting the table
+    # formatting the table
     finalsheet["A2"].value = "Time/Day"
 
     current_cell = finalsheet["A2"]
     time = 8
-    for x in range(1,12):
-        current_cell.value = str(time%12) + " To"
-        current_cell.offset(1,0).value = str((time+1)%12)
-        current_cell = current_cell.offset(2,0)
-        time+=1
+    for x in range(1, 12):
+        current_cell.value = str(time % 12) + " To"
+        current_cell.offset(1, 0).value = str((time + 1) % 12)
+        current_cell = current_cell.offset(2, 0)
+        time += 1
 
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     current_cell = finalsheet["B1"]
     for x in range(5):
         current_cell.value = days[x]
-        current_cell = current_cell.offset(0,1)
+        current_cell = current_cell.offset(0, 1)
 
-    return wb,finalsheet
+    return wb, finalsheet
+
 
 def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment=None):
     top = Border(top=border.top)
@@ -189,20 +198,22 @@ def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment
             for c in row:
                 c.fill = fill
 
-def ask_question(question,choices):
+
+def ask_question(question, choices):
     while True:
         print(question)
-        for x in range(1,len(choices)+1):
-            print("{0}) - {1}".format(x,choices[x-1]))
+        for x in range(1, len(choices) + 1):
+            print("{0}) - {1}".format(x, choices[x - 1]))
         response = input(">")
         try:
-            if int(response)<len(choices)+1:
-                return int(response)-1
+            if int(response) < len(choices) + 1:
+                return int(response) - 1
         except ValueError:
             print("Please enter a valid choice")
 
+
 if __name__ == "__main__":
-    #Load xlsx file
+    # Load xlsx file
     while True:
         try:
             print("Please input filename for the timetable")
@@ -211,13 +222,13 @@ if __name__ == "__main__":
             break
         except FileNotFoundError:
             print("please check the filename provided")
-    
-    sheet = ask_question("Please select a sheet for your year",wb.sheetnames)
+
+    sheet = ask_question("Please select a sheet for your year", wb.sheetnames)
     print(sheet)
     worksheet = wb[wb.sheetnames[sheet]]
     print(worksheet)
     batches = find_all_batches(worksheet)
-    batch = ask_question("Please select your batch",batches)
-    batch_cell = find_batch(worksheet,batches[batch])
-    finalworkbook = get_timetable(worksheet,batch_cell)
+    batch = ask_question("Please select your batch", batches)
+    batch_cell = find_batch(worksheet, batches[batch])
+    finalworkbook = get_timetable(worksheet, batch_cell)
     finalworkbook.save("{0}.xlsx".format(batches[batch]))
