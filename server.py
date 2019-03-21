a2a57f55a3a42e5bfe80ace50111b4e42c5de309
Draguve <ritwikpatil1998@gmail.com>
2019-03-21 16:10:05 +0530

    Added list for years and their classes

--- server.py
+++ server.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask,url_for
 from colorhash import ColorHash
 import sqlite3
 app = Flask(__name__)
 
-@app.route('/<file_name>/<table_name>')
-def give_table(file_name:str,table_name:str):
-    return get_html_table(file_name,table_name)
+table_file = 'time_tables.db'
 
-def get_html_table(year:str, batch:str):
-    day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    years = {
+years = {
         "1a" : "1STYEARA",
         "1b": "1STYEARB",
         "2a": "2NDYEARA",
@@ -19,8 +15,32 @@ def get_html_table(year:str, batch:str):
         "4": "4THYEAR",
         "mca": "MEM.TECHMSCMCA"
     }
+
+@app.route('/')
+def list_all_years():
+    response = ""
+    for year in years:
+        response += "<a href='{}'>{}</a><br>".format(url_for('class_in_year',year=year),years[year])
+    return response
+
+@app.route('/<year>/<batch>/')
+def give_table(year:str,batch:str):
+    return get_html_table(year,batch)
+
+@app.route('/<year>/')
+def class_in_year(year:str):
+    conn = sqlite3.connect(table_file)
+    res = conn.execute("SELECT name FROM sqlite_master WHERE type='table' and name like '%{}%';".format(years[year]))
+    response = ""
+    for name in res:
+        batch_name = name[0].split("_")[-1]
+        response += "<a href='{}'>{}</a><br>".format(url_for('give_table',year=year,batch=batch_name),batch_name)
+    return response
+
+def get_html_table(year:str, batch:str):
+    day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     table_name = "t{}_{}".format(years[year],batch.upper())
-    conn = sqlite3.connect('time_tables.db')
+    conn = sqlite3.connect(table_file)
     table_string = '''<table>
         {}
         </table>'''
@@ -36,7 +56,7 @@ def get_html_table(year:str, batch:str):
         data = '<td><b>{}</b></td>'.format(day[i])
         time = 8
         while time < 19:
-            period = conn.execute('SELECT * FROM {} WHERE DAY = {} AND START_TIME = {}'.format(table_name, i, time)).fetchone()
+            period = conn.execute('SELECT * FROM {} WHERE DAY = {} AND START_TIME = {};'.format(table_name, i, time)).fetchone()
             if period is not None:
                 #TODO: fix this nonsense
                 color = ColorHash(str(period[5][:-1]).strip().upper()).hex
